97476230af4ad6984ca2d0c3c57e10d8764dfb04
Thomas Ballinger <thomasballinger@gmail.com>
2012-07-08 23:08:24 -0400

    start async server

--- asyncproxy/eventloop.py
+++ asyncproxy/eventloop.py
@@ -1,8 +1,18 @@
-"""General Event Loop with Kqueue"""
+"""HTTP Proxy Event Loop with Kqueue"""
 
-import select
+import socket
 
+import kq
 
-class Loop(object):
-    def __init__(self):
-        j
+kq = kq.KQ()
+
+class AsyncProxy(object):
+    def __init__(self, port=8000, address='localhost'):
+        self.address = address
+        self.port = port
+        self.listensock = socket.socket()
+        self.listensock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listensock.bind((address, port))
+
+    def loop():
+        pass
